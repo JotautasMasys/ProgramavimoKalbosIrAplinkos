1e24c8a58ef8c2628f935f82117cc537d39fd337
JotautasMasys <mjotautas25@gmail>
2023-03-22 16:46:18 +0200

    linkIsIndex

--- Lab1.py
+++ Lab1.py
@@ -10,7 +10,7 @@ app = Flask(__name__,static_url_path='/')
 #Pirma svetaine
 @app.route("/")
 def mano_funkcija():
-    return ("Labas, šis puslapis yra eksperimentinis - jame nieko nebus iškyrus šie žodžiai ir žodis 'Labas'")
+    return ("Labas, šis puslapis yra eksperimentinis. Eikite i regisrtracija ir notes:")
 
 #Antra svetaine
 @app.route("/test")
@@ -19,7 +19,7 @@ def test_route():
         plus_one()
     return render_template('./index.html', var=plus_one())
 
-#trecia svetaine
+#trecia svetaine index.html
 @app.route("/debug")
 def plus_one():
     global variable
@@ -77,59 +77,7 @@ def select_from_db():
     cur = conn.cursor()
     cur.execute(queryString).fetchall()
     return array
-#-------------------------------------------------------------------------------------------------------------------------------------
-#Penkta svetaine/ NEVEIKIA
-@app.route("/register")
-def register():
-    if(request.method == "POST"):
-        global array
-        args = request.form.get("reg1")
-        if(args):
-            array.append(args)
-            insert_into_REG(args)
-            print(array)
-            return render_template('./register.html', note=select_from_REG())
-    else:
-        return render_template('./register.html', note=select_from_REG())
-
-def creationREG():
-    global connection
-    cursor=connection.cursor()
-    createRegTableString="""CREATE TABLE IF NOT EXISTS Sheets (
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        Name TEXT NOT NULL
-    )"""
-
-    cursor.execute(createRegTableString)
-    cursor.execute(createRegNotesTableString)
-
-createRegNotesTableString="""CREATE TABLE IF NOT EXISTS register (
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        SheetId INTEGER ID,
-        Header TEXT,
-        Text TEXT,
-        FOREIGN KEY (SheetId) REFERENCES Sheets(Id)
-    )"""
-
-def insert_into_REG(note):
-    conn=sqlite3.connect("./RegNotesDatabase.db")
-    queryString="""
-        INSERT INTO Sheets (Name) VALUES (?)
-    """
-    cur = conn.cursor()
-    cur.execute(queryString, (note,))
-    conn.commit()
-
-def select_from_REG():
-    conn=sqlite3.connect("./RegNotesDatabase.db")
-    queryString="""
-        SELECT name FROM Sheets
-    """
-    cur = conn.cursor()
-    cur.execute(queryString).fetchall()
-    return array
-
-#------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
 #Svetainiu ijungimo kodas:
 if __name__ =="__main__":
     creationDB()
